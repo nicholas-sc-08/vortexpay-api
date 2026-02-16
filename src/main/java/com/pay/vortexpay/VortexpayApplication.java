0f8cda65162ee42d04f8a8fde2faf62762e81ccb
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-02-16 17:43:16 -0300

    feat: initial project structure

---
+++ src/main/java/com/pay/vortexpay/VortexpayApplication.java
@@ -0,0 +1,13 @@
+package com.pay.vortexpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pay/vortexpay/VortexpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.vortexpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
