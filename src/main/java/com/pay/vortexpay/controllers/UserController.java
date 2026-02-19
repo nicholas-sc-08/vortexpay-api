b66823c193a6dffbd173ced02c7da2303fa97000
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-02-19 11:48:07 -0300

    feat: initial user CRUD

--- src/main/java/com/pay/vortexpay/controllers/UserController.java
+++ src/main/java/com/pay/vortexpay/controllers/UserController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pay.vortexpay.dtos.request.UserCreateDTO;
+import com.pay.vortexpay.dtos.request.UserUpdateDTO;
 import com.pay.vortexpay.dtos.response.UserResponseDTO;
 import com.pay.vortexpay.services.UserService;
 
@@ -44,6 +46,13 @@ public class UserController {
     public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserCreateDTO dto) {
         UserResponseDTO user = userService.createUser(dto);
         return ResponseEntity.status(201).body(user);
+    }
+
+    @Operation(summary = "Update user", description = "Allows partial updates to the user profile. If a new password is provided, it is automatically hashed using BCrypt before being persisted in the database.")
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable UUID id, @RequestBody UserUpdateDTO dto) {
+        UserResponseDTO user = userService.updateUser(id, dto);
+        return ResponseEntity.status(200).body(user);
     }
 
     @Operation(summary = "Delete user by ID", description = "Delete permanently the credentials of user access. This opreation it's irreversible and follows the LGPD guidelines to delete the data of account.")
