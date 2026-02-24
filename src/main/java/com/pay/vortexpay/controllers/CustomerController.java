46b77e3805b4beb2193dd3253318ba7d02938e39
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-02-24 18:52:06 -0300

    feat: update customer controller

--- src/main/java/com/pay/vortexpay/controllers/CustomerController.java
+++ src/main/java/com/pay/vortexpay/controllers/CustomerController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pay.vortexpay.dtos.request.CustomerCreateDTO;
+import com.pay.vortexpay.dtos.request.CustomerUpdateDTO;
 import com.pay.vortexpay.dtos.response.CustomerResponseDTO;
 import com.pay.vortexpay.services.CustomerService;
 
@@ -44,6 +46,12 @@ public class CustomerController {
     public ResponseEntity<CustomerResponseDTO> createCustomer(@RequestBody CustomerCreateDTO dto) {
         CustomerResponseDTO customer = customerService.createCustomer(dto);
         return ResponseEntity.status(201).body(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerResponseDTO> updateCustomer(@PathVariable UUID id, @RequestBody CustomerUpdateDTO dto) {
+        CustomerResponseDTO customer = customerService.updateCustomer(id, dto);
+        return ResponseEntity.status(200).body(customer);   
     }
 
     @DeleteMapping
